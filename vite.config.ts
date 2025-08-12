1c7bf8f57787020f5472716049225367b5eabd33
Ganom <8338284+Ganom@users.noreply.github.com>
2025-08-12 06:53:50 -0400

    project: configure Netlify deployment and update Vite build settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/koko-tts.github.io/' : '/',
   plugins: [
     vue(),
   ],
@@ -19,6 +20,7 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks: {
